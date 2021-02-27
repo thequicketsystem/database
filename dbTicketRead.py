7790849b7bdef7c6e8b22f99f9980785efa74f5e
archippos <deceb.z@gmail.com>
2021-02-27 16:17:26 -0600

    clearUsed, conditional stuff for readTicket
    none of it works!!!!!

--- dbTicketRead.py
+++ dbTicketRead.py
@@ -1,17 +1,37 @@
 import mariadb
 import sys
 
+#function to read out incoming tickets
 def readTicket(cur, incomingID):
     try:
         cur.execute("UPDATE guests SET used=1 WHERE ticketID=? AND used=0", (incomingID,))
-        print(incomingID, "has been set to USED.")
+        #currently only says there's no result set
+        result = cur.fetchall()
+        if result != 0:
+            print(incomingID, "has been set to USED.")
+        else:
+            print(f"ERROR: DUPLICATE TICKET ENTRY")
+            return
     except mariadb.Error as e:
         print(f"Error updating ticket: {e}")
         sys.exit(1)
 
     return
 
+#function to set all used fields to 0 for testing purposes (not working)
+def clearUsed(cur):
+    try:
+        for (ticketID) in cur:
+            cur.execute("UPDATE guests SET used=0")
+            print(f"ID reset: {ticketID}")
+    except mariadb.Error as e:
+        print(f"Error resetting database: {e}")
+        sys.exit(1)
+
+    return
+
 def main():
+    #set up our db connection
     try:
         conn = mariadb.connect(
         user="root",
@@ -22,19 +42,28 @@ def main():
         print(f"Error connecting to MariaDB Platform: {e}")
         sys.exit(1)
 
+    #establish our cursor
     print("We have connected to the database.")
     cur = conn.cursor()
 
+    #loop to continuously check tickets against the db
     while True:
+        #get an incoming ticket. later this will be real
         incomingID = str(input("Simlulated ticket is: "))
-
+        
         if (incomingID == "exit") or (incomingID == "Exit"):
+            #LET ME OOOOOOOOOOUT AAAAAAHHHHHHHH
             break
+        elif (incomingID == "wipe") or (incomingID == "reset") or (incomingID == "clear"):
+            clearUsed(cur)
+        #otherwise, read the ticket
         else:
             readTicket(cur, incomingID)
 
+    #close out our connection now that we're all done
     conn.close()
 
     return
 
 main()
+        
