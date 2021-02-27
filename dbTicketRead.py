f230d30445569fc440fe77b76170b7345138fa86
archippos <59776069+archippos@users.noreply.github.com>
2021-02-27 14:09:07 -0600

    more stuff

--- dbTicketRead.py
+++ dbTicketRead.py
@@ -11,11 +11,26 @@ except mariadb.Error as e:
     print(f"Error connecting to MariaDB Platform: {e}")
     sys.exit(1)
 
-print("DEBUG: connected")
+print("We have connected to the database.")
 
 cur = conn.cursor()
-print("DEBUG: cursor made")
+#print("DEBUG: cursor made")
 
 #now we need to figure out logic for handling incoming IDs
-incomingID = 0    #temp variable
+incomingID = 0   #temp variable
+pastID = 0
 
+#TODO: make these stmts modular
+cur.execute("USE quicketsystem")
+print("Now using database quicketsystem")
+
+incomingID = input("Simlulated ticket is: ")
+
+try:
+    cur.execute("SELECT FOR UPDATE * FROM guests WHERE ticketID=? AND used=0", (incomingID,))
+    print("DEBUG: select thingy done")
+except mariadb.Error as e:
+    print(f"Error checking ticket: {e}")
+    sys.exit(1)
+
+conn.close()
